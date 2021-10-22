7534ebc10d9b40913e7656f1443b5fd2959a4d27
BH <74618590+harringt23@users.noreply.github.com>
2021-10-22 12:09:56 -0700

    initial

--- app/src/main/java/com/example/gogame/GoGame/GoLocalGame.java
+++ app/src/main/java/com/example/gogame/GoGame/GoLocalGame.java
@@ -4,8 +4,21 @@ import com.example.gogame.GameFramework.LocalGame;
 import com.example.gogame.GameFramework.actionMessage.GameAction;
 import com.example.gogame.GameFramework.players.GamePlayer;
 
-public class GoLocalGame extends LocalGame {
-
+/* GoLocalGame
+ * A class that knows how to play the game Go. The data in this class represent
+ * the state of a game. The state represented by an instance of this class can
+ * be a complete state (as might be used by the main Go game activity) or a partial
+ * state as it would be seen from the perspective of an individual player.
+ *
+ * Each game of Go has a unique state definition, so this abstract base class
+ * has little inherent functionality.
+ *
+ * @author Brynn Harrington
+ * @version October 22
+ */
+public abstract class GoLocalGame extends LocalGame {
+    // initialize a tage for logging the current local game
+    private static final String TAG = "LocalGoGame";
 
     @Override
     protected void sendUpdatedStateTo(GamePlayer p) {
