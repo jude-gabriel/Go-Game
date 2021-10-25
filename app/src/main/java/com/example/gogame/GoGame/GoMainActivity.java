23b2955bc50bf34b4afc8dc744ea84fd0c56f635
andersmi24 <andersmi24@up.edu>
2021-10-24 19:30:08 -0700

    Added ComputerPlayer1 and started on main activity

--- app/src/main/java/com/example/gogame/GoGame/GoMainActivity.java
+++ app/src/main/java/com/example/gogame/GoGame/GoMainActivity.java
@@ -4,9 +4,13 @@ import com.example.gogame.GameFramework.GameMainActivity;
 import com.example.gogame.GameFramework.LocalGame;
 import com.example.gogame.GameFramework.gameConfiguration.GameConfig;
 import com.example.gogame.GameFramework.infoMessage.GameState;
+import com.example.gogame.GameFramework.utilities.Logger;
+import com.example.gogame.GameFramework.utilities.Saving;
+import com.example.gogame.GoGame.infoMessage.GoGameState;
 
 public class GoMainActivity extends GameMainActivity {
-
+    //Tag for logging
+    private static final String TAG = "GoMainActivity"
 
     @Override
     public GameConfig createDefaultConfig() {
@@ -15,6 +19,34 @@ public class GoMainActivity extends GameMainActivity {
 
     @Override
     public LocalGame createLocalGame(GameState gameState) {
-        return null;
+        if(gameState == null){
+            return new GoLocalGame();
+        }
+        return new GoLocalGame((GoGameState) gameState);
+    }
+
+    /**
+     * saveGame, adds this games prepend to the filename
+     */
+    @Override
+    public GameState saveGame(String gameName){
+        return super.saveGame(getGameString(gameName));
+    }
+
+    /**
+     * loadGame, adds this games prepend to the desire file to open and creates the
+     * game specific state
+     * @param gameName
+     *
+     * @return The loaded GameState
+     *
+     */
+    @Override
+    public GameState loadGame(String gameName){
+        String appName = getGameString(gameName);
+        super.loadGame(appName);
+        Logger.log(TAG, "Loading: " + gameName);
+        return (GameState) new GoGameState((GoGameState) Saving.readFromFile(appName, this.getApplicationContext()));
+
     }
 }
--- app/src/main/java/com/example/gogame/GoGame/players/GoComputerPlayer1.java
+++ app/src/main/java/com/example/gogame/GoGame/players/GoComputerPlayer1.java
@@ -1,7 +1,12 @@
 package com.example.gogame.GoGame.players;
 
 import com.example.gogame.GameFramework.infoMessage.GameInfo;
+import com.example.gogame.GameFramework.infoMessage.NotYourTurnInfo;
 import com.example.gogame.GameFramework.players.GameComputerPlayer;
+import com.example.gogame.GameFramework.utilities.Logger;
+import com.example.gogame.GoGame.goActionMessage.GoMoveAction;
+
+import java.util.Random;
 
 public class GoComputerPlayer1 extends GameComputerPlayer {
 
@@ -12,6 +17,22 @@ public class GoComputerPlayer1 extends GameComputerPlayer {
 
     @Override
     protected void receiveInfo(GameInfo info) {
+       //make random variable
+        Random rand = new Random();
+        if(info instanceof NotYourTurnInfo) return;
+        //log message?
+        Logger.log("GoComputer", "Computers Turn");
 
+        // pick random values from 0-9 for the x and y values
+        int xVal = rand.nextInt(9);
+        int yVal = rand.nextInt(9);
+
+        // delayed for a second to make the computer think were thinking
+        sleep(1);
+
+        // send the move to the game object, if it is not valid this method
+        // will continue to be called until a valid position is chosen
+        Logger.log("GoComputer", "Sending move");
+        game.sendAction(new GoMoveAction(this, xVal, yVal));
     }
 }
