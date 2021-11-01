32ae2258c985d40c7884db3e4b3fba3df2222618
tashchuk23 <tashchuk23@up.edu>
2021-11-01 11:54:53 -0700

    commit to pull testing

--- app/src/main/java/com/example/gogame/GoGame/views/GoSurfaceViewFlipped.java
+++ app/src/main/java/com/example/gogame/GoGame/views/GoSurfaceViewFlipped.java
@@ -1,12 +1,13 @@
 package com.example.gogame.GoGame.views;
 
 import android.content.Context;
+import android.util.AttributeSet;
 
 public class GoSurfaceViewFlipped extends GoSurfaceView{
     private static final String TAG = "GoSurfaceView";
 
-    public GoSurfaceViewFlipped(Context context) {
+    public GoSurfaceViewFlipped(Context context, AttributeSet attrs) {
 
-        super(context);
+        super(context,attrs);
     }
 }
