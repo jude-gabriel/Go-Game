7076486718a04da228462071c3dce1b45fa460b9
andersmi24 <andersmi24@up.edu>
2021-11-11 16:42:28 -0800

    Working on testHandicap

--- app/src/test/java/com/example/gogame/GoGameTests.java
+++ app/src/test/java/com/example/gogame/GoGameTests.java
@@ -476,19 +476,20 @@ public class GoGameTests {
         Stone[][] expectedStones = new Stone[9][9];
 
         //create two stones in position 2,2 and 6,6 that are white
-        Stone whiteStone1 = new Stone(2.0f, 2.0F);
-        Stone whiteStone2 = new Stone(6.0f, 6.0F);
+        //Stone whiteStone1 = new Stone(2.0f, 2.0F);
+        //Stone whiteStone2 = new Stone(6.0f, 6.0F);
 
-        whiteStone1.setStoneColor(Stone.StoneColor.WHITE);
-        whiteStone2.setStoneColor(Stone.StoneColor.WHITE);
+        expectedStones[2][2].setStoneColor(Stone.StoneColor.WHITE);
+        expectedStones[6][6].setStoneColor(Stone.StoneColor.WHITE);
 
         // add the stones to array
-        expectedStones[2][2] = whiteStone1;
-        expectedStones[6][6] = whiteStone2;
+        ///expectedStones[2][2] = whiteStone1;
+        //expectedStones[6][6] = whiteStone2;
 
 
         //check to see stones appear on the board
-        assertEquals("Stones are on positions 2,2 and 6,6", expectedStones, goGameState.getGameBoard());
+        assertEquals("Stones are on positions 2,2", expectedStones[2][2].getStoneColor(), goGameState.getGameBoard()[2][2].getStoneColor());
+        assertEquals("Stones are on positions 6,6", expectedStones[6][6].getStoneColor(), goGameState.getGameBoard()[6][6].getStoneColor());
 
     }
 
