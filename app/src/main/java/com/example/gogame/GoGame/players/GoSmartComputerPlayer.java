9e553cc90958c180f6f83a6da6a41a0a49f1f4d4
BH <74618590+harringt23@users.noreply.github.com>
2021-12-02 18:28:35 -0800

    added algorithm information

--- app/src/main/java/com/example/gogame/GoGame/players/GoSmartComputerPlayer.java
+++ app/src/main/java/com/example/gogame/GoGame/players/GoSmartComputerPlayer.java
@@ -13,7 +13,34 @@ import com.example.gogame.GameFramework.players.GameComputerPlayer;
  *
  * @author Brynn Harrington
  * @version November 30 2021
- * @source https://www.moderndescartes.com/essays/implementing_go/
+ * @source https://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-4-alpha-beta-pruning/?ref=lbp
+ *
+ * PSEUDOCODE FOR MINIMAX W/ ALPHA-BETA PRUNING
+	// first method call
+	minimax(0, 0, true, -INFINITY, +INFINITY)
+
+	// algorithmic implementation
+  	minimax(node, depth, isMaximizingPlayer, alpha, beta):
+    if node is a leaf node :
+        return value of the node
+    if isMaximizingPlayer :
+        bestVal = -INFINITY
+        for each child node :
+            value = minimax(node, depth+1, false, alpha, beta)
+            bestVal = max( bestVal, value)
+            alpha = max( alpha, bestVal)
+            if beta <= alpha:
+                break
+        return bestVal
+    else :
+        bestVal = +INFINITY
+        for each child node :
+            value = minimax(node, depth+1, true, alpha, beta)
+            bestVal = min( bestVal, value)
+            beta = min( beta, bestVal)
+            if beta <= alpha:
+                break
+        return bestVal
  */
 public class GoSmartComputerPlayer extends GameComputerPlayer {
 	/* LOGGING TAGS */
