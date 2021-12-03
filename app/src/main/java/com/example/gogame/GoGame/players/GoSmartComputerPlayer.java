e55c05c99752e565ba6dea83c0bbf463c5b467ed
BH <74618590+harringt23@users.noreply.github.com>
2021-12-02 18:54:13 -0800

    created smart AI move object

--- app/src/main/java/com/example/gogame/GoGame/players/GoSmartComputerPlayer.java
+++ app/src/main/java/com/example/gogame/GoGame/players/GoSmartComputerPlayer.java
@@ -1,21 +1,6 @@
-package com.example.gogame.GoGame.players;
+ /* PSEUDOCODE FOR MINIMAX W/ ALPHA-BETA PRUNING
+  * @source https://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-4-alpha-beta-pruning/?ref=lbp
 
-import com.example.gogame.GameFramework.infoMessage.GameInfo;
-import com.example.gogame.GameFramework.players.GameComputerPlayer;
-
-/**
- * A computerized Go player that recognizes an immediate capture of the
- * opponent or a possible capture from the other opponent, and plays
- * appropriately. If there is not an immediate capture (which it plays)
- * or loss (which it blocks), it moves randomly.
- *
- * The algorithm utilized to maximize difficulty is the Minimax Algorithm.
- *
- * @author Brynn Harrington
- * @version November 30 2021
- * @source https://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-4-alpha-beta-pruning/?ref=lbp
- *
- * PSEUDOCODE FOR MINIMAX W/ ALPHA-BETA PRUNING
 	// first method call
 	minimax(0, 0, true, -INFINITY, +INFINITY)
 
@@ -42,9 +27,80 @@ import com.example.gogame.GameFramework.players.GameComputerPlayer;
                 break
         return bestVal
  */
-public class GoSmartComputerPlayer extends GameComputerPlayer {
+package com.example.gogame.GoGame.players;
+
+import com.example.gogame.GameFramework.infoMessage.GameInfo;
+import com.example.gogame.GameFramework.players.GameComputerPlayer;
+
+
+
+/**
+ * SmartAIMove
+ *
+ * this class represents a move by the smart AI from the minimax alpha-beta pruning algorithm
+ *
+ */
+class SmartAIMove
+{
+	/* INSTANCE VARIABLES FOR SMART AI MOVE */
+	int row;
+	int col;
+	int score;
+
+	/**
+	 * default constructor
+	 *
+	 * sets an empty constructor
+	 *
+	 */
+	public SmartAIMove(){}//SmartAIMove
+
+	/**
+	 * score constructor
+	 *
+	 * sets the move's score from the passed in score
+	 *
+	 * @param s - the score of the move
+	 */
+	public SmartAIMove(int s) { score = s; }//SmartAIMove
+
+	/**
+	 * setting constructor
+	 *
+	 * sets the move's row, column, and score from the passed in values
+	 *
+	 * @param r - the row of the move
+	 * @param c - the column of the move
+	 * @param s - the score of the move
+	 */
+	public SmartAIMove(int r, int c, int s)
+	{
+		row = r;
+		col = c;
+		score = s;
+	}//SmartAIMove
+}
+/**
+ * A computerized Go player that recognizes an immediate capture of the
+ * opponent or a possible capture from the other opponent, and plays
+ * appropriately. If there is not an immediate capture (which it plays)
+ * or loss (which it blocks), it moves randomly.
+ *
+ * The algorithm utilized to maximize difficulty is the Minimax Algorithm with
+ * alpha-beta pruning.
+ *
+ * @author Brynn Harrington
+ * @version December 2 2021
+ */
+public class GoSmartComputerPlayer extends GameComputerPlayer
+{
 	/* LOGGING TAGS */
 	private static final String TAG = "GoSmartComputerPlayer";
+
+	/* INSTANCE VARIABLES FOR SMART AI */
+	private final int SCORE = 100;
+	private final int ALPHA = Integer.MAX_VALUE;	// represents the maximum value for alpha
+	private final int BETA = Integer.MIN_VALUE;		// represents the maximum value for beta
 
 	/**
 	 * constructor
@@ -66,4 +122,6 @@ public class GoSmartComputerPlayer extends GameComputerPlayer {
 	protected void receiveInfo(GameInfo info)
 	{
 	}//receiveInfo
+
+
 }//GoSmartComputerPlayer
